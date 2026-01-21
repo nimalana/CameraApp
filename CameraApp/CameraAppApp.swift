7fadbb0b145f7260d6d8cc09d1bd1365bbfdedb7
Nimalan Arulvelan <nimalan.arulvelan@gmail.com>
2026-01-21 12:03:03 -0500

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by Nimalan Arulvelan on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
